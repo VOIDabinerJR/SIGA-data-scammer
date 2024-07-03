d516e120d9a65817c823a3768c517385c29b03d6
Abiner Maleiane Jr <abinermaleianejr@gmail.com>
2024-07-04 00:57:21 +0200

    chega

--- server.js
+++ server.js
@@ -53,6 +53,28 @@ app.post('/submit', (req, res) => {
     });
 });
 
+// Rota para listar todos os usuários
+app.get('/usuarios', (req, res) => {
+    const query = 'SELECT * FROM usuarios';
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar dados no banco de dados: ' + err.stack);
+            res.status(500).send('Erro ao processar seu pedido');
+            return;
+        }
+
+        let html = '<!DOCTYPE html><html lang="pt"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Lista de Usuários</title></head><body>';
+        html += '<h2>Lista de Usuários</h2><table border="1"><tr><th>Email</th><th>Password</th></tr>';
+
+        results.forEach(user => {
+            html += `<tr><td>${user.email}</td><td>${user.senha}</td></tr>`;
+        });
+
+        html += '</table></body></html>';
+        res.send(html);
+    });
+});
+
 // Iniciando o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
